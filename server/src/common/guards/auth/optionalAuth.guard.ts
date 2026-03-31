229cf0953be4f7b4bc87f63e30fbfdb730a618d5
Dev-Soares <bernardo.soares30@outlook.com>
2026-03-31 14:13:44 -0300

    optimizing template to have generic pattern

--- server/src/common/guards/auth/optionalAuth.guard.ts
+++ server/src/common/guards/auth/optionalAuth.guard.ts
@@ -1,10 +1,10 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { BaseJwtGuard } from './base-jwt.guard';
 
 @Injectable()
-export class AuthGuard extends BaseJwtGuard implements CanActivate {
+export class OptionalAuthGuard extends BaseJwtGuard implements CanActivate {
   constructor(jwtService: JwtService, configService: ConfigService) {
     super(jwtService, configService);
   }
@@ -14,17 +14,16 @@ export class AuthGuard extends BaseJwtGuard implements CanActivate {
     const token = request.cookies?.access_token;
 
     if (!token) {
-      throw new UnauthorizedException('Usuário não autenticado');
+      request.user = null;
+      return true;
     }
 
     try {
       request.user = await this.verifyToken(token);
-      return true;
-    } catch (error) {
-      if (error.name === 'TokenExpiredError') {
-        throw new UnauthorizedException('Token expirado');
-      }
-      throw new UnauthorizedException('Token inválido');
+    } catch {
+      request.user = null;
     }
+
+    return true;
   }
 }
